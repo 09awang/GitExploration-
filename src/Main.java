d690b4e52804e7d99d77d8a7bf11a91913796652
Karl Arabit <127karabit@frhsd.com>
2025-09-08 10:34:09 -0400

    Saying hello to Alston Wang

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi my name is Alston Wang");
+        System.out.println("Hi Alston Wang, I'm Karl!");
     }
 }
